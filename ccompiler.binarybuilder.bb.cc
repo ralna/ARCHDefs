e395fe46462d74002d63e6079257b64f65f3658c
Alexis Montoison <alexis.montoison@polymtl.ca>
2022-11-07 19:10:03 -0500

    Update BinaryBuilder files

--- ccompiler.binarybuilder.bb.cc
+++ ccompiler.binarybuilder.bb.cc
@@ -1,12 +1,10 @@
 # C and C++ compilers for BinaryBuilder
 
-CC='$CC'
 CCBASIC='-c -fPIC'
 CCISO='-ansi -pedantic'
 CCDEBUG=#-DDEBUG_GALAHAD
 CCFFLAGS='-lgfortran -lm'
 
-CXX='$CXX'
 CXXBASIC='-c -std=c++11 -fPIC'
 CXXOPT=-O2
 CXXNOOPT=-O0
